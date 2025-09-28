537e8765717c3312248e582d7951ec487ad62f58
sanitycheck <c0dem4chine@gmail.com>
2025-09-28 09:15:37 -0400

    Remove cached build artifacts

--- src/Result.zig
+++ src/Result.zig
@@ -6,14 +6,16 @@ const result_property = @import("ResultProperty.zig");
 pub const ResultProperty = result_property.ResultProperty;
 
 pub const ResultVTable = struct {
-    resultProperties: *const fn (*anyopaque) anyerror![]const ResultProperty,
+    resultProperties: *const fn (*anyopaque) Result.ResultError![]const ResultProperty,
 };
 
 pub const Result = struct {
+    pub const ResultError = anyerror;
+
     ptr: *anyopaque,
     vtable: *const ResultVTable,
 
-    pub fn resultProperties(self: Result) anyerror![]const ResultProperty {
+    pub fn resultProperties(self: Result) ResultError![]const ResultProperty {
         return self.vtable.resultProperties(self.ptr);
     }
 
@@ -25,15 +27,17 @@ pub const Result = struct {
     }
 };
 
+pub const ResultObjectError = Result.ResultError || std.mem.Allocator.Error;
+
 pub const ResultObject = struct {
     ptr: *const anyopaque,
     vtable: *const VTable,
 
     pub const VTable = struct {
-        resultProperties: *const fn (*const anyopaque, Allocator) anyerror![]ResultProperty,
+        resultProperties: *const fn (*const anyopaque, Allocator) ResultObjectError![]ResultProperty,
     };
 
-    pub fn resultProperties(self: ResultObject, allocator: Allocator) anyerror![]ResultProperty {
+    pub fn resultProperties(self: ResultObject, allocator: Allocator) ResultObjectError![]ResultProperty {
         return self.vtable.resultProperties(self.ptr, allocator);
     }
 };
@@ -42,7 +46,7 @@ const legacy_result_object_vtable = ResultObject.VTable{
     .resultProperties = legacyResultProperties,
 };
 
-fn legacyResultProperties(ctx: *const anyopaque, allocator: Allocator) anyerror![]ResultProperty {
+fn legacyResultProperties(ctx: *const anyopaque, allocator: Allocator) ResultObjectError![]ResultProperty {
     const self: *const Result = @ptrCast(@alignCast(ctx));
     const props = try self.vtable.resultProperties(self.ptr);
     const copy = try allocator.alloc(ResultProperty, props.len);
